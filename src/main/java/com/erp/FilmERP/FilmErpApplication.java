b8ea4c6ed8ea6640589d042f72808989ce2ee051
Salma Picazo Tió <picazo.tio.salma@alumnat.copernic.cat>
2023-03-02 10:15:59 +0100

    Commit primers fitxers

---
+++ src/main/java/com/erp/FilmERP/FilmErpApplication.java
@@ -0,0 +1,13 @@
+package com.erp.FilmERP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmErpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erp/FilmERP/FilmErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erp.FilmERP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
